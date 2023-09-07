d110e30cde4ca31af9230c5dc6ace012c5d5edcd
The Free Mavens <thefreemavens@gmail.com>
2023-09-07 19:32:37 +0300

    Update (work in progress)

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -14,71 +14,72 @@
       --[@supports(backdrop-filter:blur(0px))]:dark:bg-grey-980/70
     ">
 
-      <hr class="mb-8">
+      <hr class="mb-8 border-t-8">
 
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 w-full overscroll-contain">
 
-        <section class="bg-white/0 h-full flex justify-start items-start">
-          <ul class="mt-4 tracking-wide bg-white/0">
+        <section class="h-full w-full flex justify-start items-start">
+          <ul class="mt-4 tracking-wide block w-full">
             <li class="mb-2 sm:mb-4 lg:mb-6 font-bold text-xs uppercase text-grey-975 dark:text-white">Base</li>
             <li
               v-for="(menuItem, index) in menuItems1" :key="index"
               class="my-0 py-2"
             >
-              <NuxtLink :to="menuItem.url" @click="closeModal()" class="group font-mono uppercase text-sm">
-                <span class="hover:underline hover:underline-offset-8">{{ menuItem.title }}</span>
-                <span class="mt-0.5 ms-2 opacity-0 group-hover:opacity-100">--></span>
+              <NuxtLink :to="menuItem.url" @click="closeModal()" class="group --font-mono --uppercase flex items-center py-1 transition-colors duration-100 ease-in-out text-sm hover:text-white hover:bg-black dark:hover:bg-white dark:hover:text-black">
+                <span class="flex-1">{{ menuItem.title }}</span>
+                <span class="mx-2 opacity-0 group-hover:opacity-100">--></span>
               </NuxtLink>
             </li> 
           </ul>
         </section>
 
-        <section class="bg-white/0 h-full flex justify-start items-start">
-          <ul class="mt-4 tracking-wide bg-white/0">
+        <section class="h-full w-full flex justify-start items-start">
+          <ul class="mt-4 tracking-wide block w-full">
             <li class="mb-2 sm:mb-4 lg:mb-6 font-bold text-xs uppercase text-grey-975 dark:text-white">Data</li>
             <li
               v-for="(menuItem, index) in menuItems2" :key="index"
               class="my-0 py-2"
             >
-              <NuxtLink :to="menuItem.url" @click="closeModal()" class="group font-mono uppercase text-sm">
-                <span class="hover:underline hover:underline-offset-8">{{ menuItem.title }}</span>
-                <span class="mt-0.5 ms-2 flex-1 opacity-0 group-hover:opacity-100">--></span>
+              <NuxtLink :to="menuItem.url" @click="closeModal()" class="group --font-mono --uppercase flex items-center py-1 transition-colors duration-100 ease-in-out text-sm hover:text-white hover:bg-black dark:hover:bg-white dark:hover:text-black">
+                <span class="flex-1">{{ menuItem.title }}</span>
+                <span class="mx-2 opacity-0 group-hover:opacity-100">--></span>
               </NuxtLink>
             </li> 
           </ul>
         </section>
 
-        <section class="bg-white/0 h-full flex justify-start items-start">
-          <ul class="mt-4 tracking-wide bg-white/0">
+        <section class="h-full w-full flex justify-start items-start">
+          <ul class="mt-4 tracking-wide block w-full">
             <li class="mb-2 sm:mb-4 lg:mb-6 font-bold text-xs uppercase text-grey-975 dark:text-white">Join</li>
             <li
               v-for="(menuItem, index) in menuItems3" :key="index"
               class="my-0 py-2"
             >
-              <NuxtLink :to="menuItem.url" @click="closeModal()" class="group font-mono uppercase text-sm">
-                <span class="hover:underline hover:underline-offset-8">{{ menuItem.title }}</span>
-                <span class="mt-0.5 ms-2 flex-1 opacity-0 group-hover:opacity-100">--></span>
+              <NuxtLink :to="menuItem.url" @click="closeModal()" class="group --font-mono --uppercase flex items-center py-1 transition-colors duration-100 ease-in-out text-sm hover:text-white hover:bg-black dark:hover:bg-white dark:hover:text-black">
+                <span class="flex-1">{{ menuItem.title }}</span>
+                <span class="mx-2 opacity-0 group-hover:opacity-100">--></span>
               </NuxtLink>
             </li> 
           </ul>
         </section>
 
 
-        <section class="bg-white/0 h-full flex justify-start items-start">
-          <ul class="mt-4 tracking-wide bg-white/0">
+        <section class="h-full w-full flex justify-start items-start">
+          <ul class="mt-4 tracking-wide block w-full">
             <li class="mb-2 sm:mb-4 lg:mb-6 font-bold text-xs uppercase text-grey-975 dark:text-white">Misc</li>
             <li
               v-for="(menuItem, index) in menuItems4" :key="index"
               class="my-0 py-2"
             >
-              <NuxtLink :to="menuItem.url" @click="closeModal()" class="group font-mono uppercase text-sm">
-                <span class="hover:underline hover:underline-offset-8">{{ menuItem.title }}</span>
-                <span class="mt-0.5 ms-2 flex-1 opacity-0 group-hover:opacity-100">--></span>
+              <NuxtLink :to="menuItem.url" @click="closeModal()" class="group --font-mono --uppercase flex items-center py-1 transition-colors duration-100 ease-in-out text-sm hover:text-white hover:bg-black dark:hover:bg-white dark:hover:text-black">
+                <span class="flex-1">{{ menuItem.title }}</span>
+                <span class="mx-2 opacity-0 group-hover:opacity-100">--></span>
               </NuxtLink>
             </li> 
           </ul>
         </section>
       </div>
+      <hr class="mt-8">
     </nav>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="container mx-auto">
-    <div class="mx-2">
+    <section class="mx-2">
       <!-- <hr class="my-10 mx-2 border-black"> -->
       <!-- <hr class="my-10 mx-2 border-black"> -->
       <!-- HERO -->
@@ -17,7 +17,7 @@
           flex flex-col
       ">
       Discover your true power and learn how to use it 
-      <span class="--text-red --dark:text-emerald mt-[8%] text-6xl sm:text-7xl md:text-8xl lg:text-7xl xl:text-8xl">---></span>
+      <span class="mt-[8%] text-6xl sm:text-7xl md:text-8xl lg:text-7xl xl:text-8xl">---></span>
       </h1>
 
       <ul class="
@@ -25,6 +25,7 @@
         grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3
         gap-8
         border-b
+        mb-8
       ">
 
 
@@ -38,6 +39,7 @@
           v-for="(principle, index) in principles" :key="index"
           class="
             group
+            --bg-black
             flex justify-center items-center
             aspect-square
             cursor-pointer
@@ -76,7 +78,62 @@
         </div> -->
 
       </ul>
-    </div>
+    </section>
+
+    <!-- TLDR -->
+    <section class="mx-2">
+      <h1
+        class="
+          transition-transform duration-[1600ms] ease-out
+          mx-2 py-10 mb-8 xl:pe-36
+          border-b
+          font-bold
+          text-6xl sm:text-7xl md:text-8xl lg:text-7xl xl:text-8xl
+          tracking-tighter
+          flex flex-col
+      ">
+      Stop acting like a slave. <br> Be a Maven.
+      <span class="mt-[8%] text-6xl sm:text-7xl md:text-8xl lg:text-7xl xl:text-8xl">---></span>
+      </h1>
+    </section>
+
+    <!-- TLDR -->
+    <section class="mx-2">
+      <h1
+        class="
+          transition-transform duration-[1600ms] ease-out
+          mx-2 py-10 mb-8 xl:pe-36
+          border-b
+          font-bold
+          text-6xl sm:text-7xl md:text-8xl lg:text-7xl xl:text-8xl
+          tracking-tighter
+          flex flex-col
+      ">
+      They are watching you because they are afraid of you.
+      <span class="mt-[8%] text-6xl sm:text-7xl md:text-8xl lg:text-7xl xl:text-8xl">---></span>
+      </h1>
+    </section>
+
+    <!-- Dev Notes -->
+    <section class="mx-4 grid grid-cols-2 lg:grid-cols-4 mb-8 gap-6">
+      <p>TLTR;</p>
+      <p>Become a founding member</p>
+      <p>De-Occult The Occult</p>
+      <p>Not info, knowledge</p>
+      <p>Learn to say the forbidden word, no</p>
+      <p>Keep Calm and learn Natural Law</p>
+      <p>Learn to decode symbolos of the past and their true meaning</p>
+      <p>Equip youtself with true knowledge</p>
+      <p>concquer yout biggest fears</p>
+      <p>Learn what true love is</p>
+      <p>Understand the direct correlation between true freedom and morality</p>
+      <p>We can show you the path, but it's your walk</p>
+      <p>Don't be a moral relativisit</p>
+      <p>Open source and free</p>
+      <p>The Matrix was a documentry</p>
+      <p>Data that matters</p>
+      <p>Work is for slaves. Creation is for free men</p>
+    </section>
   </main>
 </template>
 
