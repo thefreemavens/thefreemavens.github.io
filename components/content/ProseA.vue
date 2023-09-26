3fb1f30a1666342bcda994e8437b7e06c80ccce4
The Free Mavens <thefreemavens@gmail.com>
2023-09-26 17:43:35 +0300

    Update (work in progress)

---
+++ components/content/ProseA.vue
@@ -0,0 +1,24 @@
+<template>
+  <NuxtLink
+    :href="href"
+    :target="target"
+    class="border-b hover:border-b-0"
+  >
+    <slot />
+    <sup class="inline-block text-2xs relative start-0.5 -top-1 pe-0.5">🡵</sup>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  href: {
+    type: String,
+    default: ''
+  },
+  target: {
+    type: String,
+    default: undefined,
+    required: false
+  }
+})
+</script>
