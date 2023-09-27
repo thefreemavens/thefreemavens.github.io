4d89489be64aeece5a0939cfa137a2180815edc8
The Free Mavens <thefreemavens@gmail.com>
2023-09-27 13:54:31 +0300

    Update (work in progress)

---
+++ components/content/ProseBlockquote.vue
@@ -0,0 +1,5 @@
+<template>
+  <blockquote class="ps-8 pt-1 pb-0.5 border-s-4 mb-8">
+    <slot />
+  </blockquote>
+</template>
---
+++ components/content/ProseImg.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="w-full border-y my-12">
+    <img
+      :src="refinedSrc"
+      :alt="alt"
+      :width="width"
+      :height="height"
+      class="mx-auto py-8"
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+import { withTrailingSlash, withLeadingSlash, joinURL } from 'ufo'
+import { useRuntimeConfig, computed } from '#imports'
+
+const props = defineProps({
+  src: {
+    type: String,
+    default: ''
+  },
+  alt: {
+    type: String,
+    default: ''
+  },
+  width: {
+    type: [String, Number],
+    default: undefined
+  },
+  height: {
+    type: [String, Number],
+    default: undefined
+  }
+})
+
+const refinedSrc = computed(() => {
+  if (props.src?.startsWith('/') && !props.src.startsWith('//')) {
+    const _base = withLeadingSlash(withTrailingSlash(useRuntimeConfig().app.baseURL))
+    if (_base !== '/' && !props.src.startsWith(_base)) {
+      return joinURL(_base, props.src)
+    }
+  }
+  return props.src
+})
+</script>
