0d59b2e540c678cab629357fc63b7c95eac5b735
The Free Mavens <thefreemavens@gmail.com>
2023-09-28 12:35:15 +0300

    Update (work in progress)

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -21,13 +21,14 @@
   ">
     <!-- <AppLogomark class="w-12 h-12 mx-4 my-2 --rotate-[-30deg]"/> -->
     <div class="cursor-pointer --bg-red/20 ps-3.5 py-3 pe-1" @click="menu = !menu">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-7 h-7">
         <path fill-rule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
         <!-- <path fill-rule="evenodd" d="M3 9a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 9zm0 6.75a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" /> -->
       </svg>
     </div>
     <NuxtLink to="/" @click="menu = false" class="ps-1 pe-2 py-2 --bg-red/20">
-      <AppIcon name="tfm" class="w-[52px] h-[30px]"/>
+      <!-- <AppIcon name="tfm" class="w-[52px] h-[30px]"/> -->
+      <AppIcon name="tfm-s" class="w-[72px] h-[50px]"/>
       <!-- <AppLogotype class="w-[52px] h-auto" /> -->
     </NuxtLink>
 
--- pages/lex.vue
+++ pages/lex.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto relative">
     <div class="h-screen flex justify-center items-start">
-      <AppIcon name="lexmark" class="absolute -top-[20px] w-10 h-[30px] z-50"/>
+      <AppIcon name="lexrex" class="absolute -top-[20px] w-10 h-[30px] z-50"/>
     </div>
   </div>
 </template>
